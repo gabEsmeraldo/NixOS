876e9ff1b526b7ff6043f04c19930ef7e6ab75fe
gabesmeraldo <gabrielcesmeraldo@gmail.com>
2026-01-08 19:33:55 -0300

    added some essential apps

--- apps.nix
+++ apps.nix
@@ -6,16 +6,24 @@
     hyprshade
     fastfetch
     kitty
+    # alacritty
     zsh
     # git
     wget
     xfce.thunar
     yazi
+    kdePackages.ark
+    bibata-cursors
+    btop
+    cava
+    cbonsai
+    cmatrix
 
     # --- Editors & IDEs ---
     vim
     neovim
     vscode
+    code-cursor
     zed
     kdePackages.kate
     arduino-ide
@@ -27,15 +35,36 @@
     (lib.lowPrio openjdk17)    # Secondary
     (lib.lowPrio openjdk8)     # Legacy
     texliveTeTeX
+    openssh
+    mysql-workbench
+    mysql84
+    
 
     # --- Communication & Social ---
     discord
     vesktop
     # spotify
     anytype
+    joplin-desktop
+    steam
 
     # Life
     libreoffice
     kdePackages.okular
+    audacity
+    kdePackages.dolphin
+    qimgv
+    # multiviewer-for-f1
+
+    #Desktop only
+    # pavucontrol
+    # openrgb-with-all-plugins
+    # filezilla
+    # heroic
+    # hydralauncher
+    # lutris
+    # protonplus
+    # qbittorrent
+    nicotine-plus
   ];
 }
